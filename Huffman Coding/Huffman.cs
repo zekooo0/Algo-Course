bbfea26535cbab46f6ac48311ffd71990ec0ffcc
zekooo0 <ahmedzakria102@gmail.com>
2024-11-16 03:10:41 +0200

    Implement Huffman coding algorithm
    Modified Program.cs to initialize and use Huffman class. Added Huffman.cs with HeapNode and Huffman classes to implement Huffman coding algorithm. The Huffman class builds the tree and generates codes for each character in the input message.

---
+++ Huffman Coding/Huffman.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Huffman_Coding
+{
+
+    public class HeapNode
+    {
+        public char data;
+        public int freq;
+        public HeapNode left;
+        public HeapNode right;
+
+        public HeapNode(char data, int freq)
+        {
+            this.data = data;
+            this.freq = freq;
+            this.left = this.right = null;
+        }
+    }
+
+
+    public class Huffman
+    {
+        private char internal_char = (char) 0;
+        private PriorityQueue<HeapNode, int> minHeap = new PriorityQueue<HeapNode, int>();
+        public Hashtable codes = new Hashtable();
+        public Huffman( string message )
+        {
+            Hashtable freqHash = new Hashtable();
+
+            int i;
+            for ( i = 0; i < message.Length; i++ )
+            {
+                if ( freqHash[message[i]] == null )
+                {
+                    freqHash[message[i]] = 1;
+                }
+                else
+                {
+                    freqHash[message[i]] = (int) freqHash[message[i]] + 1;
+                }
+            }
+
+            foreach ( char k in freqHash.Keys )
+            {
+                HeapNode newNode = new HeapNode( k, (int) freqHash[k] );
+                minHeap.Enqueue( newNode, (int) freqHash[k] );
+            }
+
+            HeapNode top, left, right;
+            int newFreq;
+            while ( minHeap.Count != 1 )
+            {
+                left = minHeap.Dequeue();
+                right = minHeap.Dequeue();
+                newFreq = left.freq + right.freq;
+
+                top = new HeapNode(internal_char, newFreq );
+                top.left = left;
+                top.right = right;
+                minHeap.Enqueue( top, newFreq );
+            }
+            this.generateCodes( minHeap.Peek(), "" );
+        }
+
+        private void generateCodes( HeapNode node, string str )
+        {
+            if ( node == null )
+                return;
+
+            if ( node.data != internal_char )
+            {
+                codes[node.data] = str;
+            }
+
+            generateCodes( node.left, str + '0' );
+            generateCodes( node.right, str + '1' );
+
+        }
+
+    }
+
+}
+
--- Huffman Coding/Program.cs
+++ Huffman Coding/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main( string[] args )
         {
-            Console.WriteLine( "Hello, World!" );
+            string msg = "The output from Huffman's algorithm can be viewed as a variable length code table for encoding a source symbol. The algorithm derives this table from the estimated probability or frequency of occurrence for each possible value of the source symbol. as in other entropy encoding methods, more common symbols are generally represented using fewer bits than less common symbols. Huffman's method can be efficiently implemented, finding a code in time linear to the number of input weights if these weights are sorted. However, although optimal among methods encoding symbols separately, Huffman coding is not always optimal among all compression methods it is replaced with arithmetic coding or asymmetric numeral systems if better compression ratio is required.";
+
+            //string msg = "internet";
+            Huffman huff = new Huffman( msg );
+
+            foreach ( char k in huff.codes.Keys )
+            {
+                Console.Write( k + " " );
+                Console.WriteLine( huff.codes[k] );
+            }
         }
     }
 }

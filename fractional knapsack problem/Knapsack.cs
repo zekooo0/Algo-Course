60079aaf339538b19d805704c2741efd56817fc1
zekooo0 <ahmedzakria102@gmail.com>
2024-11-18 01:58:56 +0200

    Add fractional knapsack problem project
    Updated the solution file `Algo Course.sln` to include a new project for the "fractional knapsack problem". Added the project configuration to the global section of the solution file.
    
    Created a new project file `fractional knapsack problem.csproj` specifying the project SDK, output type, target framework, root namespace, and enabled implicit usings and nullable reference types.
    
    Added `Knapsack.cs` defining `Item` and `Knapsack` classes. The `Item` class represents an item with a name, weight, value, and value-to-weight ratio. The `Knapsack` class represents a knapsack with a maximum capacity, current capacity, total value, and a list of items. It includes methods for adding items and performing a merge sort on items based on their value-to-weight ratio.
    
    Added `Program.cs` containing the `Main` method for the project. It initializes a list of items, sorts them by value-to-weight ratio, adds items to a knapsack until it reaches its maximum capacity, and prints the sorted list of items and the contents of the knapsack.

---
+++ fractional knapsack problem/Knapsack.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fractional_knapsack_problem
+{
+    public class Item
+    {
+        public string name;
+        public double weight;
+        public double value;
+        public double ratio;
+
+        public Item( string name, double weight, double value )
+        {
+            this.name = name;
+            this.weight = weight;
+            this.value = value;
+            this.ratio = value / weight;
+        }
+    }
+
+    public class Knapsack
+    {
+        public double maxCapacity;
+        public double currentCapacity;
+        public double totalValue;
+        public List<Item> items;
+        
+        public Knapsack( int maxCapacity )
+        {
+            this.maxCapacity = maxCapacity;
+            this.currentCapacity = 0;
+            this.totalValue = 0;
+            this.items = new List<Item>();
+        }
+
+        public void add_item( Item newItem )
+        {
+            if ( newItem.weight > maxCapacity - currentCapacity )
+            {
+                double diff = maxCapacity - currentCapacity;
+                newItem.weight = diff;
+                newItem.value = diff * newItem.ratio;
+            }
+
+            currentCapacity += newItem.weight;
+            totalValue += newItem.value;
+            items.Add( newItem );  
+
+        }
+
+
+        public static void merge_sort( Item[] arr, int start, int end )
+        {
+
+            if ( end <= start )
+                return;
+            int mid = ( end + start ) / 2;
+
+            merge_sort( arr, start, mid );
+            merge_sort( arr, mid + 1, end );
+
+            merge( arr, start, mid, end );
+        }
+
+        public static void merge( Item[] arr, int start, int mid, int end )
+        {
+            int i, j, k;
+            int left_length = mid - start + 1;
+            int right_length = end - mid;
+
+            Item[] left_array = new Item[left_length];
+            Item[] right_array = new Item[right_length];
+
+            for ( i = 0; i < left_length; i++ )
+            {
+                left_array[i] = arr[start + i];
+            }
+
+            for ( j = 0; j < right_length; j++ )
+            {
+                right_array[j] = arr[mid + 1 + j];
+            }
+
+            i = 0;
+            j = 0;
+            k = start;
+
+            while ( i < left_length && j < right_length )
+            {
+                if ( left_array[i].ratio > right_array[j].ratio )
+                {
+                    arr[k] = left_array[i];
+                    i++;
+                    k++;
+                }
+                else
+                {
+                    arr[k] = right_array[j];
+                    j++;
+                    k++;
+                }
+            }
+
+            while ( i < left_length )
+            {
+                arr[k] = left_array[i];
+                i++;
+                k++;
+            }
+
+            while ( j < right_length )
+            {
+                arr[k] = right_array[j];
+                j++;
+                k++;
+            }
+
+        }
+
+
+
+    }
+}
---
+++ fractional knapsack problem/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace fractional_knapsack_problem
+{
+    internal class Program
+    {
+        static void Main( string[] args )
+        {
+            int[] values = { 4, 9, 12, 11, 6, 5 };
+            int[] weights = { 1, 2, 10, 4, 3, 5 };
+            
+            int n = weights.Length;
+            
+            List<Item> itemList = new List<Item>();
+   
+            
+            for ( int i = 0; i < n; i++ )
+            {
+                Item newItem = new Item( $"#{i}", weights[i], values[i] );
+                itemList.Add( newItem );
+            }
+            
+            Item[] items = itemList.ToArray();
+
+            // sort desc based on the ratio
+            Knapsack.merge_sort( items, 0, n - 1 );
+
+
+            
+
+            Knapsack bag = new Knapsack( 12 );
+
+            int j = 0;
+            while ( bag.currentCapacity < bag.maxCapacity )
+            {
+                bag.add_item( items[j] );
+                j++;
+            }
+
+
+            // Print the list after the sort 
+            Console.WriteLine( "Name Value Weight Ratio" );
+            foreach ( var item in items )
+            {
+                Console.WriteLine( $"{item.name} {item.value} {item.weight} {item.ratio}" );
+            }
+
+            Console.WriteLine( "=================================" );
+            Console.WriteLine( $"current capacity: {bag.currentCapacity} total value: {bag.totalValue}" );
+
+
+            foreach ( var item in bag.items )
+            {
+                Console.WriteLine( $"{item.name} {item.weight} {item.value} {item.ratio}" );
+            }
+        }
+    }
+}

3f574f56e7b177ea57f244b530ee0fe79f54cb56
zekooo0 <ahmedzakria102@gmail.com>
2024-11-23 02:50:40 +0200

    Add "DP - Stagecoach" project to solution
    Added "DP - Stagecoach" project to `Algo Course.sln` with build configurations. Created `DP - Stagecoach.csproj` targeting .NET 8.0 with implicit usings and nullable reference types enabled. Implemented dynamic programming solution in `Program.cs` for the stagecoach problem.

---
+++ DP - Stagecoach/Program.cs
@@ -0,0 +1,90 @@
+﻿namespace DP___Stagecoach
+{
+
+    public class State
+    {
+        public string From
+        {
+            get;
+            set;
+        }
+        public string To
+        {
+            get;
+            set;
+        }
+        public int Cost
+        {
+            get;
+            set;
+        }
+    }
+    internal class Program
+    {
+        static void Main( string[] args )
+        {
+            string[] labels = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J" };
+
+            int[,] data = {
+        {0, 2, 4, 3, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 7, 4, 6, 0, 0, 0},
+        {0, 0, 0, 0, 3, 2, 4, 0, 0, 0},
+        {0, 0, 0, 0, 4, 1, 5, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 1, 4, 0},
+        {0, 0, 0, 0, 0, 0, 0, 6, 3, 0},
+        {0, 0, 0, 0, 0, 0, 0, 3, 3, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 3},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 4},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+    };
+
+            int n = data.GetLength( 0 );
+            State[] states = new State[n];
+
+            states[n - 1] = new State { From = "", To = "", Cost = 0 };
+
+            int i = n - 2;
+            int j;
+            for ( ; i >= 0; i-- )
+            {
+                j = i + 1;
+                states[i] = new State { From = labels[i], To = labels[j], Cost = int.MaxValue };
+                for ( ; j < n; j++ )
+                {
+                    if ( data[i, j] == 0 )
+                        continue;
+                    int new_cost = data[i, j] + states[j].Cost;
+
+                    if ( new_cost < states[i].Cost )
+                    {
+                        states[i].To = labels[j];
+                        states[i].Cost = new_cost;
+                    }
+                }
+            }
+
+            List<string> path = new List<string>();
+            path.Add( "A" );
+            i = 0;
+            j = 0;
+
+            while ( i < states.Length )
+            {
+                if ( states[i].From == path[j] )
+                {
+                    path.Add( states[i].To );
+                    j++;
+                }
+                i++;
+            }
+
+            Console.WriteLine( $"Min Cost: {states[0].Cost}" );
+            foreach ( string str in path )
+            {
+                Console.Write( str + "\t" );
+            }
+
+        }
+
+    }
+}
